29b38a77ea93676f86f181b1f95a1c45620df3f3
Astenik <106985461+Astenik@users.noreply.github.com>
2022-11-30 21:38:31 +0400

    Update TreeNode.h

--- TreeNode.h
+++ TreeNode.h
@@ -3,14 +3,16 @@
 template <typename T>
 struct TreeNode
 {
-    TreeNode(const T& d, TreeNode<T>* l = nullptr, TreeNode<T>* r = nullptr)
+    TreeNode(const T& d, TreeNode<T>* p = nullptr, TreeNode<T>* l = nullptr, TreeNode<T>* r = nullptr)
         : data(d)
+        , parent(p)
         , left(l)
         , right(r)
     {}
     ~TreeNode() {}
     
     T data;
+    TreeNode<T>* parent;
     TreeNode<T>* left;
     TreeNode<T>* right;
 };
