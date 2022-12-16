c391c72695ba7b4a7d133c481f619f1de860f63e
Astenik <106985461+Astenik@users.noreply.github.com>
2022-12-16 20:08:31 +0400

    Update HeapMain.cpp

--- HeapMain.cpp
+++ HeapMain.cpp
@@ -30,30 +30,29 @@ int main()
     std::cout << "Heap size after heapsort " <<  h.get_size() << std::endl;
     
     std::cout << "MIN heap " << "!!!!!!!" << std::endl;
-    Heap<int> h(Min);
-    h.insert(7);
-    h.insert(5);
-    h.insert(10);
-    h.insert(14);
-    h.insert(3);
-    h.insert(25);
+    Heap<int> h1(Min);
+    h1.insert(7);
+    h1.insert(5);
+    h1.insert(10);
+    h1.insert(14);
+    h1.insert(3);
+    h1.insert(25);
     
     std::cout << "heap is" << " ";
-    h.print();
+    h1.print();
     
-    std::cout << "heap size " << h.get_size() << std::endl;
+    std::cout << "heap size " << h1.get_size() << std::endl;
     
     std::cout << "heapsort" << " ";
-    int d = h.get_size();
-    for(int i = 0; i < d; ++i)
+    int d1 = h1.get_size();
+    for(int i = 0; i < d1; ++i)
     {
-        std::cout << h.get_root() << " ";
-        h.remove_root();
+        std::cout << h1.get_root() << " ";
+        h1.remove_root();
     }
     std::cout << std::endl;
     
-    std::cout << "Heap size after heapsort " <<  h.get_size() << std::endl;
-    
+    std::cout << "Heap size after heapsort " <<  h1.get_size() << std::endl;
     
     return 0;
 }
