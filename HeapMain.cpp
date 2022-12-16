c138292881ce20c037a88f8244dec83cefa43496
Astenik <106985461+Astenik@users.noreply.github.com>
2022-12-16 20:01:10 +0400

    Update HeapMain.cpp

--- HeapMain.cpp
+++ HeapMain.cpp
@@ -4,6 +4,7 @@
 
 int main()
 {
+    std::cout << "MAX heap " << "!!!!!!!" << std::endl;
     Heap<int> h(Max);
     h.insert(7);
     h.insert(5);
@@ -15,19 +16,44 @@ int main()
     std::cout << "heap is" << " ";
     h.print();
     
+    std::cout << "heap size " << h.get_size() << std::endl;
+    
     std::cout << "heapsort" << " ";
-    for(int i = 0; i < h.get_size(); ++i)
+    int d = h.get_size();
+    for(int i = 0; i < d; ++i)
     {
         std::cout << h.get_root() << " ";
         h.remove_root();
     }
     std::cout << std::endl;
     
-    h.remove_root();
-    h.remove_root();
+    std::cout << "Heap size after heapsort " <<  h.get_size() << std::endl;
     
-    std::cout << h.get_root() << std::endl;
-    std::cout << h.get_size() << std::endl;
+    std::cout << "MIN heap " << "!!!!!!!" << std::endl;
+    Heap<int> h(Min);
+    h.insert(7);
+    h.insert(5);
+    h.insert(10);
+    h.insert(14);
+    h.insert(3);
+    h.insert(25);
+    
+    std::cout << "heap is" << " ";
+    h.print();
+    
+    std::cout << "heap size " << h.get_size() << std::endl;
+    
+    std::cout << "heapsort" << " ";
+    int d = h.get_size();
+    for(int i = 0; i < d; ++i)
+    {
+        std::cout << h.get_root() << " ";
+        h.remove_root();
+    }
+    std::cout << std::endl;
+    
+    std::cout << "Heap size after heapsort " <<  h.get_size() << std::endl;
+    
     
     return 0;
 }
