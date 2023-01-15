370f658dbc710b62aff187ce31fd1703168fcdad
Astenik <106985461+Astenik@users.noreply.github.com>
2023-01-15 14:58:41 +0400

    Update BSTMain.cpp

--- BSTMain.cpp
+++ BSTMain.cpp
@@ -5,13 +5,22 @@
 int main()
 {
     BST<int> tree;
+    BST<int> tree1;
     tree.insert(7);
     tree.insert(56);
     tree.insert(5);
     tree.insert(100);
     tree.insert(1);
     tree.insert(6);
-    tree.insert(47);
+    tree.insert(48);
+    
+    tree1.insert(7);
+    tree1.insert(56);
+    tree1.insert(5);
+    tree1.insert(100);
+    tree1.insert(1);
+    tree1.insert(6);
+    tree1.insert(47);
     
     tree.levelorder_iter();
     std::cout << std::endl;
@@ -19,6 +28,13 @@ int main()
     std::cout << "tree height is equal to: " << tree.height() << std::endl;
     std::cout << "tree width is equal to: " << tree.width() <<std::endl;
     
+    std:: << "two trees are equal: " << " ";
+    if(tree == tree1)
+        std::cout << "true" << std::endl;
+    else
+    {
+        std::cout << "false" << std::endl;
+    }
     
     return 0;
 }
